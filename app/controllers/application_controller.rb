469af7f399ccd680afc2dabe2648eb31e3cc9bcf
RianaRC <riana@sobantu.co.za>
2018-06-04 19:37:56 +0300

    vita le devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    before_action :sanitize_devise_params, if: :devise_controller?
+
+    def sanitize_devise_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phone])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phone])
+    end
+
 end
